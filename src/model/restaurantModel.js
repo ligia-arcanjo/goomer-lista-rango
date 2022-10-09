ace9333b7dcbbfbb788a2d4936162951805c8e7e
Ligia <ligiaarcanjo84@gmail.com>
2022-10-09 16:00:02 -0300

    feat: GET /restaurant/:id returns hours and address information

--- src/model/restaurantModel.js
+++ src/model/restaurantModel.js
@@ -14,4 +14,26 @@ const getRestaurantById = async (id) => {
 	return restaurant;
 };
 
-module.exports = { getAllRestaurants, getRestaurantById };
+const getRestaurantAddress = async (idRestaurant) => {
+	const [restaurantAddres] = await connection.execute(
+		`SELECT street, number, complement, city, state, postal_code
+        FROM Goomer.address
+        WHERE id_restaurant = ?`,
+		[idRestaurant]
+	);
+
+	return restaurantAddres;
+};
+
+const getRestaurantHours = async (idRestaurant) => {
+	const [hours] = await connection.execute(
+		`SELECT day_week, start, end
+        FROM Goomer.restaurant_hours
+        WHERE id_restaurant = ?`,
+		[idRestaurant]
+	);
+
+	return hours;
+};
+
+module.exports = { getAllRestaurants, getRestaurantById, getRestaurantAddress, getRestaurantHours };
--- src/service/restaurantService.js
+++ src/service/restaurantService.js
@@ -11,7 +11,25 @@ const getRestaurantById = async (idRestaurant) => {
 
 	if (restaurant.length < 1) throw new Error('Restaurant not found');
 
+	const restaurantAddress = await getRestaurantAddress(idRestaurant);
+	restaurant.address = restaurantAddress;
+
+	const restaurantHours = await getRestaurantOppeningHours(idRestaurant);
+	restaurant.hours = restaurantHours;
+
 	return restaurant;
+};
+
+const getRestaurantAddress = async (idRestaurant) => {
+	const [address] = await restaurantModel.getRestaurantAddress(idRestaurant);
+
+	return address;
+};
+
+const getRestaurantOppeningHours = async (idRestaurant) => {
+	const hours = await restaurantModel.getRestaurantHours(idRestaurant);
+
+	return hours;
 };
 
 module.exports = { getAllRestaurants, getRestaurantById };
